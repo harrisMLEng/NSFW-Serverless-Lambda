c70fc01e16adbc78f6b15aca92da7823a8c3dde4
harrisDLEng <harris47naeem@gmail.com>
2023-08-29 01:35:20 +0500

    changing requirements and classifier.py

--- classfier.py
+++ classfier.py
@@ -1,55 +1,18 @@
-# import os
-# import pydload
-# import logging
-import numpy as np
-import onnxruntime
-# from .video_utils import get_interest_frames_from_video
-# from .image_utils import load_images
-from PIL import Image as pil_image
+
+import io
+import logging
+import os
 from typing import List, Tuple
 
-import os
-import io
-import cv2
-import pydload
-import logging
 import numpy as np
-
+import onnxruntime
 from PIL import Image as pil_image
-
-if pil_image is not None:
-    _PIL_INTERPOLATION_METHODS = {
-        "nearest": pil_image.NEAREST,
-        "bilinear": pil_image.BILINEAR,
-        "bicubic": pil_image.BICUBIC,
-    }
-    # These methods were only introduced in version 3.4.0 (2016).
-    if hasattr(pil_image, "HAMMING"):
-        _PIL_INTERPOLATION_METHODS["hamming"] = pil_image.HAMMING
-    if hasattr(pil_image, "BOX"):
-        _PIL_INTERPOLATION_METHODS["box"] = pil_image.BOX
-    # This method is new in version 1.1.3 (2013).
-    if hasattr(pil_image, "LANCZOS"):
-        _PIL_INTERPOLATION_METHODS["lanczos"] = pil_image.LANCZOS
 
 
 def load_img(
-    path, grayscale=False, color_mode="rgb", target_size=None, interpolation="nearest"
+    path, grayscale=False, color_mode="rgb", target_size=(256,256), interpolation="nearest"
 ):
-    
-    if grayscale is True:
-        logging.warn("grayscale is deprecated. Please use " 'color_mode = "grayscale"')
-        color_mode = "grayscale"
-    if pil_image is None:
-        raise ImportError(
-            "Could not import PIL.Image. " "The use of `load_img` requires PIL."
-        )
-
-    if isinstance(path, (str, io.IOBase)):
-        img = pil_image.open(path)
-    # else:
-    #     path = cv2.cvtColor(path, cv2.COLOR_BGR2RGB)
-    #     img = pil_image.fromarray(path)
+    img = pil_image.open(path)
 
     if color_mode == "grayscale":
         if img.mode != "L":
@@ -62,18 +25,9 @@ def load_img(
             img = img.convert("RGB")
     else:
         raise ValueError('color_mode must be "grayscale", "rgb", or "rgba"')
-    if target_size is not None:
-        width_height_tuple = (target_size[1], target_size[0])
-        if img.size != width_height_tuple:
-            if interpolation not in _PIL_INTERPOLATION_METHODS:
-                raise ValueError(
-                    "Invalid interpolation method {} specified. Supported "
-                    "methods are {}".format(
-                        interpolation, ", ".join(_PIL_INTERPOLATION_METHODS.keys())
-                    )
-                )
-            resample = _PIL_INTERPOLATION_METHODS[interpolation]
-            img = img.resize(width_height_tuple, resample)
+    
+    width_height_tuple = (target_size[1], target_size[0])
+    img = img.resize(width_height_tuple, resample=0)
     return img
 
 
@@ -109,17 +63,7 @@ def img_to_array(img, data_format="channels_last", dtype="float32"):
 
 
 def load_images(image_paths, image_size, image_names):
-    """
-    Function for loading images into numpy arrays for passing to model.predict
-    inputs:
-        image_paths: list of image paths to load
-        image_size: size into which images should be resized
     
-    outputs:
-        loaded_images: loaded images on which keras model can run predictions
-        loaded_image_indexes: paths of images which the function is able to process
-    
-    """
     loaded_images = []
     loaded_image_paths = []
 
@@ -137,54 +81,34 @@ def load_images(image_paths, image_size, image_names):
 
 
 class Classifier:
-    """
-    Class for loading model and running predictions.
-    For example on how to use take a look the if __name__ == '__main__' part.
-    """
-
-    nsfw_model = None
 
     def __init__(self):
         """
         model = Classifier()
         """
-        self.nsfw_model = onnxruntime.InferenceSession(model_path)
+        self.nsfw_model = onnxruntime.InferenceSession('classifier_model.onnx')
+        print("Model Loaded")
 
 
     def classify(
         self,
-        image_path : str,
+        img : np.ndarray,
         batch_size : int = 4,
-        image_size : Tuple =(256, 256),
         categories=["unsafe", "safe"],
     ):
-        """
-        inputs:
-            image_paths: list of image paths or can be a string too (for single image)
-            batch_size: batch_size for running predictions
-            image_size: size to which the image needs to be resized
-            categories: since the model predicts numbers, categories is the list of actual names of categories
-        """
-        if not isinstance(image_paths, list):
-            image_paths = [image_paths]
-
-        loaded_images, loaded_image_paths = load_images(
-            image_paths, image_size, image_names=image_paths
-        )
-
-        if not loaded_image_paths:
-            return {}
-
+       
+        
         preds = []
         model_preds = []
-        while len(loaded_images):
-            _model_preds = self.nsfw_model.run(
-                [self.nsfw_model.get_outputs()[0].name],
-                {self.nsfw_model.get_inputs()[0].name: loaded_images[:batch_size]},
-            )[0]
-            model_preds.append(_model_preds)
-            preds += np.argsort(_model_preds, axis=1).tolist()
-            loaded_images = loaded_images[batch_size:]
+        _model_preds = self.nsfw_model.run(
+            [self.nsfw_model.get_outputs()[0].name],
+            {self.nsfw_model.get_inputs()[0].name: [img]},
+        )[0]
+        
+        model_preds.append(_model_preds)
+        preds += np.argsort(_model_preds, axis=1).tolist()
+        print(preds)
+        
 
         probs = []
         for i, single_preds in enumerate(preds):
@@ -197,26 +121,26 @@ class Classifier:
 
             probs.append(single_probs)
 
-        images_preds = {}
-
-        for i, loaded_image_path in enumerate(loaded_image_paths):
-            if not isinstance(loaded_image_path, str):
-                loaded_image_path = i
-
-            images_preds[loaded_image_path] = {}
-            for _ in range(len(preds[i])):
-                images_preds[loaded_image_path][preds[i][_]] = float(probs[i][_])
-
-        return images_preds
+        return preds, probs
 
 
 if __name__ == "__main__":
-    m = Classifier()
+    NSFW = Classifier()
+    img_path = ["imgs/1.png"]
+    image = load_img(img_path[0])
+    image = img_to_array(image)
+    image /= 255    
+    preds, probs = NSFW.classify(np.asarray(image))
 
-    while 1:
-        print(
-            "\n Enter single image path or multiple images seperated by || (2 pipes) \n"
-        )
-        images = input().split("||")
-        images = [image.strip() for image in images]
-        print(m.predict(images), "\n")
+    print(preds, probs)
+
+    images_preds = {}
+
+  
+    loaded_image_path = 0
+
+    images_preds[loaded_image_path] = {}
+    for _ in range(len(preds[0])):
+        images_preds[loaded_image_path][preds[0][_]] = float(probs[0][_])
+
+    print(images_preds)
